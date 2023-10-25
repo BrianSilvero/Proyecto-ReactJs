667434a7e92982f63efc0f51c5a8eabeb0f867a5
Brian Silvero <briandavidsilvero@gmail.com>
2023-10-25 00:11:40 -0300

    segunda preentrega

--- hunger/src/components/item/item.jsx
+++ hunger/src/components/item/item.jsx
@@ -1,8 +1,11 @@
+
+import { Link } from 'react-router-dom'
 const Item = ({producto}) => {
     return(
         <div>
             <p>{producto.name}</p>
             <p>${producto.precio}</p>
+            <Link to ={`/item/${producto.id}`}>Ver mas</Link>
         </div>
     )
 }
